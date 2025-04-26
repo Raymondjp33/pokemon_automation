7b2df477c9508ef06c4e25449706eeb8f6d052bb
Raymond Price <RaymondJPrice33@gmail.com>
2025-04-26 00:27:57 -0400

    Update to bot server to handle sending data on initial connect to the browser

--- backend/bot_server.py
+++ backend/bot_server.py
@@ -24,6 +24,31 @@ modified_vars = {
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
+
+@socketio.on('init_connect')
+def handle_connect():
+        try:
+            if not os.path.exists(switch1_file_path) or not os.path.exists(switch2_file_path) or not os.path.exists(stream_data_file_path):
+                return
+
+            file_data1 = {}
+            file_data2 = {}
+            file_data3 = {}
+            with open(switch1_file_path) as f1, open(switch2_file_path) as f2, open(stream_data_file_path) as f3:
+                file_data1 = json.load(f1)
+                file_data2 = json.load(f2)
+                file_data3 = json.load(f3)
+            print(f"[WebSocket] Emitting init connect")
+            socketio.emit("switch1_data", file_data1)
+            socketio.emit("switch2_data", file_data2)
+            socketio.emit("stream_data", file_data3)
+        except Exception as e:
+                print(f"Error reading file: {e}")
+
+
 def watch_files():
     global last_modified, file_data
     while True:
@@ -50,7 +75,7 @@ def handle_file_update(file_path, modified_var, emit_variable):
     except Exception as e:
             print(f"Error reading file: {e}")
 
-# http://0.0.0.0:5050
+# http://0.0.0.0:5050/
 if __name__ == '__main__':
     socketio.start_background_task(target=watch_files)
     socketio.run(app, host='0.0.0.0', port=5050)
