8187c1c6b5f0d7592c65579e9e1e488842ac7d81
Raymond Price <RaymondJPrice33@gmail.com>
2025-04-26 00:32:07 -0400

    Updated file provider logic for stats on screen

--- stream-browser/stream_browser/lib/services/file_provider.dart
+++ stream-browser/stream_browser/lib/services/file_provider.dart
@@ -1,16 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
+
+import '../models/pokemon.model.dart';
+import '../models/stream_data.model.dart';
+import '../models/switch_data.model.dart';
 
 class FileProvider with ChangeNotifier {
   FileProvider() {
     connectSocket();
+    startTimer();
   }
 
   io.Socket? socket;
 
-  Map<String, dynamic>? switch1Data;
-  Map<String, dynamic>? switch2Data;
-  Map<String, dynamic>? streamData;
+  SwitchData? switch1Data;
+  SwitchData? switch2Data;
+  StreamData? streamData;
 
   void connectSocket() {
     socket = io.io(
@@ -23,11 +30,23 @@ class FileProvider with ChangeNotifier {
 
     socket!.onConnect((_) {
       print('[Connected] to server');
+      socket!.emit('init_connect');
+    });
+
+    socket!.on('stream_data', (data) {
+      try {
+        streamData = StreamData.fromJson(data);
+        updateVariables();
+        notifyListeners();
+      } catch (e) {
+        print('Error with data $e');
+      }
     });
 
     socket!.on('switch1_data', (data) {
       try {
-        switch1Data = data as Map<String, dynamic>;
+        switch1Data = SwitchData.fromJson(data);
+        calculateSwitch1Variables();
         notifyListeners();
       } catch (e) {
         print('Error with data $e');
@@ -36,16 +55,8 @@ class FileProvider with ChangeNotifier {
 
     socket!.on('switch2_data', (data) {
       try {
-        switch2Data = data as Map<String, dynamic>;
-        notifyListeners();
-      } catch (e) {
-        print('Error with data $e');
-      }
-    });
-
-    socket!.on('stream_data', (data) {
-      try {
-        streamData = data as Map<String, dynamic>;
+        switch2Data = SwitchData.fromJson(data);
+        calculateSwitch2Variables();
         notifyListeners();
       } catch (e) {
         print('Error with data $e');
@@ -56,4 +67,87 @@ class FileProvider with ChangeNotifier {
 
     socket!.connect();
   }
+
+  late Timer timer;
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      notifyListeners();
+    });
+  }
+
+  void updateVariables() {
+    calculateSwitch1Variables();
+    calculateSwitch2Variables();
+  }
+
+  //
+  //        SWITCH 1
+  //
+  int switch1TotalShinies = 0;
+  int switch1TotalEncounters = 0;
+  double switch1AverageEncounters = 0;
+  int switch1Encounters = 0;
+  void calculateSwitch1Variables() {
+    switch1TotalShinies = (switch1Data?.pokemon.length ?? 1) - 1;
+    switch1TotalEncounters = (switch1Data?.pokemon ?? []).fold(
+      0,
+      (previousValue, element) => previousValue + (element.encounters ?? 0),
+    );
+
+    switch1AverageEncounters =
+        switch1TotalEncounters / (switch1Data?.pokemon.length ?? 1);
+    switch1Encounters = switch1CurrPokemon.encounters ?? 0;
+    notifyListeners();
+  }
+
+  //
+  //        SWITCH 2
+  //
+  int switch2LegendaryShinies = 0;
+  int switch2TotalDens = 0;
+  int totalDens = 0;
+  double switch2AverageChecks = 0;
+  int switch2Encounters = 0;
+
+  void calculateSwitch2Variables() {
+    switch2LegendaryShinies = (switch2Data?.pokemon.length ?? 1) - 1;
+    switch2TotalDens = (switch2Data?.pokemon ?? []).fold(
+      0,
+      (previousValue, element) {
+        if (element.extraData == null ||
+            element.extraData?['total_dens'] is! int) {
+          return previousValue + 0;
+        }
+
+        return previousValue + (element.extraData!['total_dens'] as int);
+      },
+    );
+
+    List<PokemonData> denPokemon = (switch2Data?.pokemon ?? [])
+        .where((e) => e.extraData?['total_dens'] != null)
+        .toList();
+
+    int totalEncounters = denPokemon.fold(
+      0,
+      (previousValue, element) => previousValue + (element.encounters ?? 0),
+    );
+
+    switch2AverageChecks = totalEncounters / denPokemon.length;
+    switch2Encounters = switch2CurrPokemon.encounters ?? 0;
+    notifyListeners();
+  }
+
+  PokemonData get switch1CurrPokemon =>
+      switch1Data?.pokemon.firstWhere(
+        (element) => element.pokemon == streamData?.switch1CurrentlyHunting,
+        orElse: () => PokemonData.emptyPokemon,
+      ) ??
+      PokemonData.emptyPokemon;
+
+  PokemonData get switch2CurrPokemon =>
+      switch2Data?.pokemon.firstWhere(
+        (element) => element.pokemon == streamData?.switch2CurrentlyHunting,
+        orElse: () => PokemonData.emptyPokemon,
+      ) ??
+      PokemonData.emptyPokemon;
 }
