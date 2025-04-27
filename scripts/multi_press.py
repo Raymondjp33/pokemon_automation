58ae7be9a400b26887988570bc270227c73e6647
Raymond Price <RaymondJPrice33@gmail.com>
2025-04-27 18:43:24 -0400

    Include count in multi press and add switch arg to make it easy to switch between switches!

--- scripts/multi_press.py
+++ scripts/multi_press.py
@@ -10,15 +10,16 @@ def main() -> int:
     # parser.add_argument('--serial', default=SERIAL_DEFAULT)
     # parser.add_argument('--serial', default='/dev/tty.usbserial-120') # Switch 2
     parser.add_argument('--serial', default='/dev/tty.usbmodem1101') # Switch 1
-    
+    parser.add_argument('--switch', type=float, default=1)
+
     args = parser.parse_args()
 
-    with serial.Serial(args.serial, 9600) as ser:
+    serial_input = '/dev/tty.usbmodem1101' if args.switch == 1 else '/dev/tty.usbserial-120'
+
+    with serial.Serial(serial_input, 9600) as ser:
         time.sleep(1)
         while True:
             user_input = input("> ").strip()
-
-
             inputs = user_input.split(' ')
 
             duration = 0.1
@@ -33,11 +34,12 @@ def main() -> int:
                 break
             
             elif user_input:  # Filter single character keys
-                ser.write(user_input[0].encode())
-                time.sleep(duration)
-                ser.write(b'.')
-                time.sleep(.05)
-                print(f"Sent: {user_input[0]}")
+                for _ in range(count):
+                    ser.write(user_input[0].encode())
+                    time.sleep(duration)
+                    ser.write(b'.')
+                    time.sleep(.05)
+                    print(f"Sent: {user_input[0]}")
 
 
 
