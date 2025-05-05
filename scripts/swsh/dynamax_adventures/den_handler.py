4f8ff29bd5fc04b735734148fe5f5ef4926e6465
Raymond Price <RaymondJPrice33@gmail.com>
2025-05-05 15:47:39 -0400

    Update currently hunting in den handler

--- scripts/swsh/dynamax_adventures/den_handler.py
+++ scripts/swsh/dynamax_adventures/den_handler.py
@@ -14,7 +14,7 @@ os.environ['TESSDATA_PREFIX'] = '/opt/homebrew/Cellar/tesseract/5.5.0/share/tess
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/Cellar/tesseract/5.5.0/bin/tesseract'
 pokemon_data_path = Path(__file__).resolve().parent.parent / 'pokemon_data.json'
 
-currently_hunting = 'Zekrom'
+currently_hunting = 'Celesteela'
 
 class DenHandler:
     def __init__(self, vid: cv2.VideoCapture, ser: serial.Serial, config: ConfigManager):
