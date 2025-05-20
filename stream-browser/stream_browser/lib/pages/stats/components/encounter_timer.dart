a7ad960bf6f786e208b658e88da8ab318d7fc198
Raymond Price <RaymondJPrice33@gmail.com>
2025-05-19 23:50:55 -0400

    Convert encounter timer to stateful and add in timer so that it rebuilds as needed

--- stream-browser/stream_browser/lib/pages/stats/components/encounter_timer.dart
+++ stream-browser/stream_browser/lib/pages/stats/components/encounter_timer.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../../../constants/app_styles.dart';
 
-class EncounterTimer extends StatelessWidget {
+class EncounterTimer extends StatefulWidget {
   const EncounterTimer({
     required this.startTime,
     required this.endTime,
@@ -15,18 +17,45 @@ class EncounterTimer extends StatelessWidget {
   final int? fontSize;
 
   @override
+  State<EncounterTimer> createState() => _EncounterTimerState();
+}
+
+class _EncounterTimerState extends State<EncounterTimer> {
+  late Timer _timer;
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (Timer timer) {
+        if (widget.endTime == null && widget.startTime != null) {
+          setState(() {});
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (startTime == null) {
+    if (widget.startTime == null) {
       return Container();
     }
 
     DateTime now = DateTime.now();
     Duration timeDifference = Duration(
-      milliseconds: endTime ?? now.millisecondsSinceEpoch - startTime!.toInt(),
+      milliseconds: widget.endTime ??
+          now.millisecondsSinceEpoch - widget.startTime!.toInt(),
     );
     return Text(
       formatDuration(timeDifference),
-      style: AppTextStyles.pokePixel(fontSize: fontSize?.toDouble() ?? 24),
+      style:
+          AppTextStyles.pokePixel(fontSize: widget.fontSize?.toDouble() ?? 24),
     );
   }
 
