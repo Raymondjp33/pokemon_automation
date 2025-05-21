48ceecf5da55ae7b4af9b53cd1d5398311699bc4
Raymond Price <RaymondJPrice33@gmail.com>
2025-05-21 14:38:33 -0400

    Update to bdsp eggs.py to move pokemon over to another box

--- scripts/bdsp/eggs.py
+++ scripts/bdsp/eggs.py
@@ -176,8 +176,7 @@ def increment_counter(caught_index=None):
     # Increment the counter
     count += 5
 
-    with counter_path.open("w") as file1:
-        file1.write(str(count))
+ 
 
     with data_path.open("r") as data_file:
         data = json.load(data_file)
@@ -190,8 +189,8 @@ def increment_counter(caught_index=None):
     for entry in data["pokemon"]:
         if entry["pokemon"] == stream_data['switch1_currently_hunting']:
             current_pokemon = entry
-            current_pokemon["encounters"] = entry["encounters"] + 5
             break
+         
     
     if (caught_index is not None):
         catches = current_pokemon["catches"]
@@ -205,7 +204,11 @@ def increment_counter(caught_index=None):
                     "caught_timestamp": int(time.time() * 1000),
                     "encounters": count_difference
                 })
-  
+    else:
+        current_pokemon["encounters"] = entry["encounters"] + 5
+        with counter_path.open("w") as file1:
+            file1.write(str(count))
+            
     with open(data_path, 'w') as data_file:
         json.dump(data, data_file, indent=4)
       
@@ -246,6 +249,35 @@ def reset_game(ser: serial.Serial, vid: cv2.VideoCapture,):
         frame = _getframe(vid)
 
     print('game loaded!')
+
+class Position(NamedTuple):
+    col: int
+    row: int
+
+def get_location(num):  
+    box_pos = num % 30
+    if (box_pos == 0):
+        box_pos = 30
+    row = int((box_pos - 1)/6) 
+    col = box_pos % 6
+    if (col == 0):
+        col = 5
+    else:
+        col = col - 1
+    
+    return Position(col=col, row=row)
+
+def make_move(ser: serial.Serial, from_pos, to_pos, move_vertical = False,):
+    difference = to_pos - from_pos
+
+    invert = True
+    if (difference >= 0):
+        invert = False
+
+    if (move_vertical):
+        _press(ser, '2' if not invert else '4', count=abs(difference), sleep_time=0.5)
+    else:
+        _press(ser, '1' if not invert else '3', count=abs(difference), sleep_time=0.5)
 
 def handle_fetch(ser: serial.Serial, vid: cv2.VideoCapture,):
     _press(ser, 's', duration=0.75, sleep_time=0.2)
@@ -309,6 +341,8 @@ def handle_process_eggs(ser: serial.Serial, vid: cv2.VideoCapture,):
 
     _press(ser, 'a', sleep_time=.5)
 
+    line_count = 5
+
     # Check for any shiny
     for x in range(5):
         _press(ser, 's', sleep_time=.75)
@@ -316,14 +350,37 @@ def handle_process_eggs(ser: serial.Serial, vid: cv2.VideoCapture,):
         if (is_shiny):
             print(f'We have a shiny at index {x}!')
             increment_counter(caught_index=x)
-            return True
-        print(f'Pokemon at index {x} is not shiny')
+            line_count = line_count - 1
+            # Pick shiny up 
+            print('Picking shiny up')
+            _press(ser, 'A', sleep_time=0.75, count=2)
+            time.sleep(1)
+            # Put in open spot
+            print('Putting in open spot')
+            _press(ser, 'w', count = x + 1, sleep_time=0.5)
+            _press(ser, 'd', sleep_time=0.5)
+            _press(ser, 'R', sleep_time=2)
+            move_location = get_location(config.get('open_slot'))
+            config.update({'open_slot': config.get('open_slot') + 1})
+
+            make_move(ser, from_pos=0, to_pos=move_location.row, move_vertical=True)
+            make_move(ser, from_pos=0, to_pos=move_location.col, move_vertical=False)
+            _press(ser, 'A', sleep_time=1.5)
+
+            # Come back
+            make_move(ser, from_pos=move_location.row, to_pos=0, move_vertical=True)
+            make_move(ser, from_pos=move_location.col, to_pos=0, move_vertical=False)
+            _press(ser, 'a', sleep_time=0.5)
+            _press(ser, 's', count = x, sleep_time=0.5)
+            _press(ser, 'L', sleep_time=2)
+        else:   
+            print(f'Pokemon at index {x} is not shiny')
 
     increment_counter(caught_index=None)
-    _press(ser, 'w', count=4, sleep_time=0.25)
+    _press(ser, 'w', count=line_count - 1, sleep_time=0.25)
 
     # Delete all non shiny
-    for _ in range(5):
+    for _ in range(line_count):
         _press(ser, 'A', sleep_time=.75)
         _press(ser, 'w', count=2, sleep_time=0.25)
         _press(ser, 'A', sleep_time=.75)
@@ -435,20 +492,8 @@ def main() -> int:
                 ser.write(b'0')
                 time.sleep(0.5)
 
-                if handle_process_eggs(ser, vid):
-                    _press(ser, 'H', duration=1)
-                    _press(ser, 's', duration=0.25)
-                    _press(ser, 'd', duration=0.25)
-                    _press(ser, 'd', duration=0.25)
-                    _press(ser, 'd', duration=0.25)
-                    _press(ser, 'd', duration=0.25)
-                    _press(ser, 'd', duration=0.25)
-                    _press(ser, 'd', duration=0.25)
-                    _press(ser, 'A', duration=1)
-                    _press(ser, 'w', duration=1)
-                    _press(ser, 'A', duration=1)
-                    write_shiny_text()
-                    return 0
+                handle_process_eggs(ser, vid)
+
                 end_time = time.time()
                 print(f'Full egg run took {(end_time-start_time):.3f}s')
                 start_time = time.time()
